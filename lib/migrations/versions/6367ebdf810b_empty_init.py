79739ef87e152699a7e7d4e930c1a7a14b400db4
Everlyn <gachuieve32@gmail.com>
2023-12-11 17:04:53 +0300

    alembic empty init

--- lib/migrations/versions/6367ebdf810b_empty_init.py
+++ lib/migrations/versions/6367ebdf810b_empty_init.py
@@ -1,8 +1,8 @@
 """Empty Init
 
-Revision ID: 224297e13728
+Revision ID: 6367ebdf810b
 Revises: 
-Create Date: 2023-12-11 16:59:20.653822
+Create Date: 2023-12-11 17:03:29.692074
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '224297e13728'
+revision = '6367ebdf810b'
 down_revision = None
 branch_labels = None
 depends_on = None
