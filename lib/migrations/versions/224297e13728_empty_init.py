2b7ccc1026ee84c147d267e97de2c328a7f947e6
Everlyn <gachuieve32@gmail.com>
2023-12-11 17:00:25 +0300

    alembic empty init

---
+++ lib/migrations/versions/224297e13728_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 224297e13728
+Revises: 
+Create Date: 2023-12-11 16:59:20.653822
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '224297e13728'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
